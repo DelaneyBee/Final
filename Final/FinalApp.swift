d4c9822a29b44061f847964aa932aba85e229846
Delaney Blaszinski <Dablasz6@gmail.com>
2024-11-25 09:35:52 -0600

    Initial Commit

---
+++ Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by Delaney Blaszinski on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
