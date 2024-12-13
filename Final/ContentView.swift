4ecb70fa5d8f1f1e073fdbd63ef354e42aed95a5
Delaney Blaszinski <Dablasz6@gmail.com>
2024-12-13 01:27:02 -0600

    finished the game mecanics

--- Final/ContentView.swift
+++ Final/ContentView.swift
@@ -12,9 +12,11 @@ struct ContentView: View {
     @State private var randomValue = 0
     @State private var turnScore = 0
     @State private var Guess = ""
-    @State private var PricePresOne = 200
     @State private var sliderValue = 0.0
+    
     @State private var TextSlider = ""
+    @State private var moves = Array(repeating: "", count: 1)
+    @State private var xTurn = true
     var body: some View {
         ZStack {
             NavigationView {
@@ -23,21 +25,22 @@ struct ContentView: View {
                     ZStack {
                         HStack {
                             Image("shopping \(randomValue)")
-                                .resizable().frame(width: 170, height: 170)
+                                .resizable().frame(width: 160, height: 176)
                                 .rotationEffect(.degrees(rotation))
                                 .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
-                            
                         }
                     }
+                    .buttonStyle(CustomButtonStyle())
                         ZStack {
                             Slider(value: $sliderValue, in: 1...6)
                             Text(TextSlider).frame(width: 130, height: 120).offset(x:0, y: -70)
-                                .font(.title)
+                               
                                 .padding()
                             Spacer()
                         }
-                        .frame(width: 200, height: 190)
+                        .frame(width: 200, height: 190).font(Font.custom("Times New Roman", size: 30))
                         .offset(x: 0, y: -130)
+                     
                     //All the decorations
                     ZStack {
                         Image("Red Peppermint").resizable().frame(width: 60, height: 60).offset(x: 150, y: -370)
@@ -68,9 +71,13 @@ struct ContentView: View {
                         .buttonStyle(CustomButtonStyle())
                     }
                     .offset(x: -4, y: 270)
-                    
-                    
-                    
+                    VStack {
+                        NavigationLink("Check Price", destination: InstructionsView3())
+                       
+                        
+                    }
+                    .offset(x:0, y: 180)
+                    .buttonStyle(CustomButtonStyle())
                 }
             }
         }
@@ -97,8 +104,6 @@ struct ContentView: View {
                 TextSlider = HowMuch4(language: "" )
                 break
             case 5..<6:
-                
-                
                 TextSlider = HowMuch5(langauge: "" )
                 break
             default:
@@ -106,6 +111,7 @@ struct ContentView: View {
             
             }
         }
+        
     }
         struct InstructionsView: View {
             var body: some View {
@@ -118,7 +124,7 @@ struct ContentView: View {
                         Image("Lights").resizable().frame(width: 400, height: 320).offset(x: 0, y: 130)
                     }
                     ZStack {
-                        Image("Presents 1").resizable().frame(width: 100, height: 100).offset(x: 0, y: -120)
+                        Image("Presents 3").resizable().frame(width: 100, height: 100).offset(x: -5, y: -120)
                         Image("Presents 2").resizable().frame(width: 100, height: 100).offset(x: 70, y: -120)
                         Image("Presents 2").resizable().frame(width: 100, height: 100).offset(x: -70, y: -120)
                         Image("Wreath").resizable().frame(width: 200, height: 200).offset(x: 10, y: 130)
@@ -129,17 +135,14 @@ struct ContentView: View {
                         Text("Instructions").font(Font.custom("Times New Roman", size: 30)).bold()
                         Spacer()
                         VStack {
-                            Text("To win: a player must guess the correct price of a present out of three prices.")
-                                .font(Font.custom("Times New Roman", size: 25))
+                            Text("To win: a player must guess the correct price of the present out of the choices on the slider.")
+                                .font(Font.custom("Times New Roman", size: 20))
                                 .padding()
-                            Text("Points are counted based on number of correct guesses.").offset(x: 0, y: -25)
-                                .font(Font.custom("Times New Roman", size: 25))
+                            Text("To check if your guess is correct, click the 'check price' button.").multilineTextAlignment(.center)
+                                .font(Font.custom("Times New Roman", size: 20))
                             
                             Spacer()
-                            VStack {
-                                NavigationLink("Info On The Presents", destination: InstructionsView2()).font(Font.custom("Times New Roman", size: 30)).bold()
-                                
-                            }
+                            
                             Spacer()
                             Spacer()
                             Spacer()
@@ -149,6 +152,130 @@ struct ContentView: View {
                 }
             }
         }
+    
+    struct InstructionsView3: View {
+        func HeadPhonePrice(langauge: String) -> String {
+            return "200"
+        }
+        func UggPrice(language: String) -> String {
+            return "150"
+        }
+        func robePrice(language: String) -> String {
+            return "80"
+        }
+        func BracePrice(language: String) -> String {
+            return "90"
+        }
+        @State var sliderValue4 = 0.0
+        @State var sliderValue5 = 0.0
+        @State var sliderValue2 = 0.0
+        @State var sliderValue3 = 0.0
+        @State var TextSlider2 = ""
+        @State var TextSlider3 = ""
+        @State var TextSlider4 = ""
+        @State var TextSlider5 = ""
+        var body: some View {
+           
+            ZStack {
+                Color.green.opacity(0.7).ignoresSafeArea()
+                VStack {
+                    
+                    Image("Lights").resizable().frame(width: 400, height: 320).offset(x: 0, y: -225)
+                        .padding()
+                    Image("Lights").resizable().frame(width: 400, height: 320).offset(x: 0, y: 160)
+                }
+                ZStack {
+                    Image("Presents 1").resizable().frame(width: 100, height: 100).offset(x: 0, y: -120)
+                    Image("Presents 2").resizable().frame(width: 100, height: 100).offset(x: 150, y: 350)
+                    Image("Presents 2").resizable().frame(width: 100, height: 100).offset(x: -150, y: 350)
+                    Image("Wreath").resizable().frame(width: 200, height: 200).offset(x: 0, y: -260)
+                    Image("Holly").resizable().frame(width: 100, height: 100).offset(x: -100, y: -210)
+                    Image("Holly").resizable().frame(width: 100, height: 100).offset(x: 100, y: -210)
+                }
+                ZStack {
+                    Slider(value: $sliderValue2, in: 0...1)
+                    Text(TextSlider2).frame(width: 130, height: 120).offset(x:-10, y: -30)
+                         }
+                .font(Font.custom("Times New Roman", size: 30)).offset(x:0, y:-80)
+                .frame(width: 130, height: 120).offset(x:-120, y: 10)
+                Image("shopping 1").resizable().frame(width :100, height :100).offset(x:-120, y:10)
+                
+                
+                
+                ZStack {
+                    Slider(value: $sliderValue3, in: 0...1)
+                    Text(TextSlider3).frame(width: 130, height: 120).offset(x:-10, y: 30)
+                }
+                .font(Font.custom("Times New Roman", size: 30)).offset(x:0, y:80)
+                .frame(width: 130, height: 120).offset(x:-120, y: 170)
+                Image("shopping 2").resizable().frame(width :100, height :100).offset(x:-120, y:170)
+                
+                ZStack {
+                    Slider(value: $sliderValue4, in: 0...1)
+                    Text(TextSlider4).frame(width: 130, height: 120 ).offset(x: -10, y: -30)
+                }
+                .font(Font.custom("Times New Roman", size: 30)).offset(x: 220, y:-80)
+                .frame(width: 130, height: 120).offset(x:-120, y: 10)
+                Image("shopping 3").resizable().frame(width :100, height :100).offset(x:120, y:10)
+                
+                ZStack {
+                    Slider(value: $sliderValue5, in: 0...1)
+                    Text(TextSlider5).frame(width: 130, height: 120 ).offset(x: 0, y: 0)
+                }
+                .font(Font.custom("Times New Roman", size: 30)).offset(x: 220, y:80)
+                .frame(width: 130, height: 120).offset(x:-110, y: 170)
+                Image("shopping 4").resizable().frame(width :100, height :100).offset(x:120, y:170)
+            }
+            
+            
+            
+            .onChange(of: sliderValue2) {
+                switch sliderValue2{
+                case 0..<1:
+                    TextSlider2 = HeadPhonePrice(langauge: "")
+                    func HeadPhonePrice(langauge: String) -> String {
+                        return "$200"
+                    }
+                default:
+                    break
+                }
+            }
+            .onChange(of: sliderValue3) {
+                switch sliderValue3 {
+                case 0..<1:
+                    TextSlider3 = UggPrice(langauge: "")
+                    func UggPrice(langauge: String) -> String {
+                        return "$150"
+                    }
+                default:
+                    break
+                }
+            }
+            .onChange(of: sliderValue4) {
+                switch sliderValue4 {
+                case 0..<1:
+                    TextSlider4 = robePrice(langauge: "")
+                    func robePrice(langauge: String) -> String {
+                        return "$70"
+                    }
+                default:
+                    break
+                }
+            }
+            .onChange(of: sliderValue5) {
+                switch sliderValue5 {
+                case 0..<1:
+                    TextSlider5 = BracePrice(langauge: "")
+                    func BracePrice(langauge: String) -> String {
+                        return "$90"
+                    }
+                default:
+                    break
+                }
+            }
+            
+        }
+    }
         struct InstructionsView2: View {
             var body: some View {
                 ZStack {
@@ -200,12 +327,12 @@ struct ContentView: View {
     
    
     func HowMuch3(language : String) -> String {
-       return "$90"
+       return "$100"
     }
     
     
     func HowMuch4(language: String) -> String {
-        return "$100"
+        return "$150"
     }
     
     
@@ -213,7 +340,9 @@ struct ContentView: View {
         return "$200"
     }
         
-        
+    func HeadPhonePrice(langauge: String) -> String {
+        return "200"
+    }
         
     
 }
